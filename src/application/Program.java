e9fa16ced688d023c5c7a30a48eb3109819d6a7d
Alessandrojp-dev <alessandrojp_dev@hotmail.com>
2025-07-21 16:30:50 -0300

    Department entity class

---
+++ src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		//TESTE
+		Department obj = new Department(1, "Books");
+		System.out.println(obj);
+
+	}
+
+}
